f9ad91d61ffc7f1dd281cb4b44e306600d24d64a
dkameroon <kvv200328@gmail.com>
2022-10-01 18:12:57 +0300

    Moving Warehouse Miner

---
+++ Assets/Scripts/Miner/WarehouseMiner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarehouseMiner : BaseMiner
+{
+    public Deposit ElevatorDeposit { get; set; }
+    public Vector3 ElevatorDepositLocation { get; set; }
+    public Vector3 WarehouseLocation { get; set; }
+
+    private int _walkAnimation = Animator.StringToHash("Walk");
+
+    protected override void MoveMiner(Vector3 newPosition)
+    {
+        base.MoveMiner(newPosition);
+        _animator.SetBool(_walkAnimation,true);
+    }
+
+    public override void OnClick()
+    {
+        RotateMiner(-1);
+        MoveMiner(ElevatorDepositLocation);
+    }
+}
--- Assets/Scripts/Warehouse/Warehouse.cs
+++ Assets/Scripts/Warehouse/Warehouse.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Warehouse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Prefab")]
+    [SerializeField] private WarehouseMiner minerPrefab;
+
+    [Header("Extras")]
+    [SerializeField] private Deposit elevatorDeposit;
+    [SerializeField] private Transform elevatorDepositLocation;
+    [SerializeField] private Transform warehouseDepositLocation;
+
+    private void Start()
     {
-        
+        AddMiner();    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddMiner()
     {
-        
+        WarehouseMiner newMiner = Instantiate(minerPrefab, warehouseDepositLocation.position, quaternion.identity);
+        newMiner.ElevatorDeposit = elevatorDeposit;
+        newMiner.ElevatorDepositLocation = new Vector3(elevatorDepositLocation.position.x, warehouseDepositLocation.position.y);
+        newMiner.WarehouseLocation = new Vector3(warehouseDepositLocation.position.x,warehouseDepositLocation.position.y);
     }
 }
